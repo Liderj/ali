548bc3b54703520cb1e5a8954edb2d5bc6355542
lider <1050515074@qq.com>
2018-06-14 23:44:44 +0800

    删除性能监控apm以及自动化api文档Swagger

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,13 +1,10 @@
 "use strict";
 
-const pkginfo = require("../../package.json");
 exports.welcome = ctx => {
   // BUSINESS LOGIC
   const data = {
-    name: pkginfo.name,
-    version: pkginfo.version,
-    description: pkginfo.description,
-    author: pkginfo.author
+    name: "lider",
+    version: "v1"
   };
 
   ctx.res.fail(200, "Hello, API!", data);
